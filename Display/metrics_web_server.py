24cb1451a405b0bce1a3df7c153b9eefe3e0c8bc
Frank Seelmann <frankseelmann123@gmail.com>
2025-04-06 12:57:31 -0400

    run isort and black formatter on Python files

--- Display/metrics_web_server.py
+++ Display/metrics_web_server.py
@@ -8,12 +8,12 @@
 # TODO:                                                                                   #
 ###########################################################################################
 
-from flask import Flask, request, render_template
-from flask_socketio import SocketIO, emit
-from web_server_database_interface import generate_datasets
-import read_database
 import subprocess as sp
 
+import read_database
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from web_server_database_interface import generate_datasets
 
 # Set up the Flask instance as well as the socket IO
 app = Flask(__name__)
@@ -21,24 +21,25 @@ socketio = SocketIO(app)
 
 
 # index.html is the standard default case.
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/gather', methods=['POST'])
+@app.route("/gather", methods=["POST"])
 def gather_report():
-    return render_template('gather.html')
+    return render_template("gather.html")
 
 
-@app.route('/user_report', methods=['POST'])
+@app.route("/user_report", methods=["POST"])
 def user_report():
     datasets = generate_datasets()
     datasets = read_database.read_metrics()
     max_datapoints = max((len(lst) for lst in datasets.values()), default=0)
-    return render_template('reports.html', datasets=datasets, max_datapoints=max_datapoints)
+    return render_template(
+        "reports.html", datasets=datasets, max_datapoints=max_datapoints
+    )
 
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, host='127.0.0.1')
-
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="127.0.0.1")
