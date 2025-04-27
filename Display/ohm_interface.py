43527cf723cacc5bf15115e4b1e5242f5062f309
A2AFelsen <michael.t.huffer@gmail.com>
2025-04-27 11:29:28 -0400

    Adding prune support and testing of some end of life changes.

--- Display/ohm_interface.py
+++ Display/ohm_interface.py
@@ -21,11 +21,45 @@ def is_metrics_running():
     return False
 
 
-def call_executable(years=0, months=0, weeks=0, days=0, prune_now=False):
+def get_metrics_exe():
     parent_dir = os.path.dirname(os.getcwd())
-    metrics_exe = os.path.join(parent_dir, "OpenHardwareMonitor.exe")
+    return os.path.join(parent_dir, "OpenHardwareMonitor.exe")
+
+
+def call_executable(years="1", months="0", weeks="0", days="0"):
+    if not years.isdigit():
+        years = "1"
+    if not months.isdigit():
+        months = "0"
+    if not weeks.isdigit():
+        weeks = "0"
+    if not days.isdigit():
+        days = "0"
+
+    metrics_exe = get_metrics_exe()
+    #metrics_call = [metrics_exe, "--lifetime", f"{str(years)}y", f"{str(months)}m", f"{str(weeks)}w", f"{str(days)}d"]
+    #metrics_call = [metrics_exe, "--lifetime", f"{str(years)}y{str(months)}m{str(weeks)}w{str(days)}d"]
+    #metrics_call = [metrics_exe, "--lifetime", f"{str(days)}d"]
+    metrics_call = [metrics_exe]
+    print(metrics_call)
     if os.path.exists(metrics_exe):
-        subprocess.run(metrics_exe, cwd=parent_dir, capture_output=True, text=True, check=True)
+        parent_dir = os.path.dirname(os.getcwd())
+        output = subprocess.run(metrics_call, cwd=parent_dir, capture_output=True, text=True, check=True)
+        print(output.stdout)
         return "Metrics Started"
     else:
-        return "Can't Find Metrics"
+        return "Can't Find Metrics Executable"
+
+
+def prune_data():
+    metrics_exe = get_metrics_exe()
+    metrics_call = [metrics_exe, "prune-now"]
+    if os.path.exists(metrics_exe):
+        parent_dir = os.path.dirname(os.getcwd())
+        output = subprocess.run(metrics_call, cwd=parent_dir, capture_output=True, text=True, check=True)
+        if output.stderr:
+            return output.stderr
+        else:
+            return output.stdout
+    else:
+        return "Can't Find Metrics Executable"
